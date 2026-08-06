634765c606b7b6578d78bcb67583af8213e4c092
amanocha87 <amanocha87@gmail.com>
2026-08-06 23:34:32 +0100

    Use official Amplify logo assets

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import smallLogo from '../assets/images/amplify_favicon_1786042841289.jpg';
-import largeLogo from '../assets/images/amplify_logo_1786042824020.jpg';
 
 interface LogoProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   variant?: 'small' | 'large';
   className?: string;
 }
 
-export default function Logo({ variant = 'small', className = '', ...props }: LogoProps) {
-  const imgSrc = variant === 'large' ? largeLogo : smallLogo;
-  
+export default function Logo({
+  variant = 'small',
+  className = '',
+  ...props
+}: LogoProps) {
+  const imgSrc =
+    variant === 'large'
+      ? '/amplify-logo-large.png'
+      : '/amplify-logo-small.png';
+
   return (
-    <img 
-      src={imgSrc} 
-      alt="Amplify Logo" 
+    <img
+      src={imgSrc}
+      alt="Amplify"
       className={`object-contain ${className}`}
-      {...props} 
+      {...props}
     />
   );
 }
