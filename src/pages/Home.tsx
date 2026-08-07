2682df559dccb2a89cc72dcc6b35f660ab459783
amanocha87 <amanocha87@gmail.com>
2026-08-07 22:30:18 +0100

    Simplify homepage and remove duplicate sections
    Removed unused components from the Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,15 @@
 import Hero from '../components/Hero';
-import BusinessesWeHelpThrive from '../components/BusinessesWeHelpThrive';
-import AmplifyMethod from '../components/AmplifyMethod';
 import CoreSolutions from '../components/CoreSolutions';
-import CaseStudies from '../components/CaseStudies';
 import Testimonials from '../components/Testimonials';
-import Story from '../components/Story';
 import ReferralRewards from '../components/ReferralRewards';
-import PricingCTA from '../components/PricingCTA';
 import FinalCTA from '../components/FinalCTA';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <BusinessesWeHelpThrive />
-      <AmplifyMethod />
       <CoreSolutions />
-      <CaseStudies />
       <Testimonials />
-      <Story />
-      <PricingCTA />
       <ReferralRewards />
       <FinalCTA />
     </>
