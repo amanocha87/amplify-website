a8c85fcc5d5e36ad46cdbc48b80415cb825638ea
amanocha87 <amanocha87@gmail.com>
2026-08-09 23:00:56 +0100

    Add referral-rewards route

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import CookiePolicy from './pages/CookiePolicy';
 import TermsConditions from './pages/TermsConditions';
 import Accessibility from './pages/Accessibility';
 import Contact from './pages/Contact';
+import ReferralProgram from './pages/ReferralProgram';
 import CaseStudies from './pages/CaseStudies';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
@@ -42,6 +43,7 @@ export default function App() {
             <Route path="/terms" element={<TermsConditions />} />
             <Route path="/accessibility" element={<Accessibility />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/referral-rewards" element={<ReferralProgram />} />
             <Route path="/case-studies" element={<CaseStudies />} />
           </Routes>
         </main>
