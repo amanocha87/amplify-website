06600697498d4cdcadc4a33b127405a56b2a5cc0
amanocha87 <amanocha87@gmail.com>
2026-08-10 20:38:01 +0100

    Add Pricing page with ROI calculator

--- src/pages/PricingPage.tsx
+++ src/pages/PricingPage.tsx
@@ -1,1 +1,11 @@
+import Pricing from '../components/Pricing';
+import ROICalculator from '../components/ROICalculator';
 
+export default function PricingPage() {
+  return (
+    <div className="pt-24">
+      <Pricing />
+      <ROICalculator />
+    </div>
+  );
+}
