de6404aa406497e05c4210653971301b4c4bcdd3
amanocha87 <amanocha87@gmail.com>
2026-08-11 23:50:06 +0100

    Add Supabase client connection

--- src/components/lib/supabase.ts
+++ src/components/lib/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://ttakqtnfpkxheqsisavr.supabase.co';
+const supabaseAnonKey = 'sb_publishable_ZFJgJ_HyACUSIOm-zUC_QA_wleyd66L';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
