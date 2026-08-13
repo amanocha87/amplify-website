727c6c5202e2ecaa4dd09aa4c5e6592c6b0249e0
amanocha87 <amanocha87@gmail.com>
2026-08-13 21:59:26 +0100

    Refactor Home component structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,17 @@ import ReactiveToProactive from '../components/ReactiveToProactive';
 import Hero from '../components/Hero';
 import CoreSolutions from '../components/CoreSolutions';
 import Testimonials from '../components/Testimonials';
-import ReferralRewards from '../components/ReferralRewards';
 import FinalCTA from '../components/FinalCTA';
 
 export default function Home() {
   return (
-<>
-  <Hero />
-<AmplifyMethod />
-<ReactiveToProactive />
-<CoreSolutions />
-  <Testimonials />
-  <ReferralRewards />
-  <FinalCTA />
-</>
+    <>
+      <Hero />
+      <AmplifyMethod />
+      <ReactiveToProactive />
+      <CoreSolutions />
+      <Testimonials />
+      <FinalCTA />
+    </>
   );
 }
